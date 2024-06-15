2aaabb325c4232f1cc4d172cfe89d3e3f9a889c0
Sorelul <sorinake_eu@yahoo.com>
2024-06-16 00:26:13 +0300

    BE - companii

---
+++ application/controllers/Companii.php
@@ -0,0 +1,48 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Companii extends CI_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Companies_model', "Companies");
+    }
+
+    public function index()
+    {
+        redirect('companii/lista');
+    }
+
+    public function lista()
+    {
+
+        $search_term = null;
+
+        $city = null;
+        $city_info = null;
+
+        if ($this->input->get('search')) {
+            $search_term = $this->input->get('search');
+        }
+
+        if ($this->input->get('city')) {
+            $city = $this->input->get('city');
+            $city_info = $this->Companies->getCityById($city);
+        }
+
+        $companies = $this->Companies->getAllCompanies($city, $search_term);
+
+
+        $data = array(
+            'companies' => $companies,
+            'city_info' => $city_info,
+            'search' => $search_term
+        );
+
+        $this->load->view('template/header', array('title' => "Listă companii"));
+        $this->load->view('companii/list', $data);
+        $this->load->view('template/footer');
+    }
+}
--- application/models/Companies_model.php
+++ application/models/Companies_model.php
@@ -2,10 +2,40 @@
 class Companies_model extends CI_Model
 {
 
+    function getAllCompanies($city = null, $search_term = null)
+    {
+        if ($city) {
+            $this->db->where('companies_city', $city);
+        }
+
+        if ($search_term) {
+            $this->db->like('companies_name', $search_term);
+            $this->db->or_like('companies_email', $search_term);
+            $this->db->or_like('companies_cui', $search_term);
+        }
+
+        $this->db->join('cities', 'cities.id_city = companies.companies_city', 'left');
+
+        $result = $this->db->get('companies');
+        return $result->result();
+    }
+
     function getCompanyByUser($companies_admin)
     {
         $this->db->where('companies_admin', $companies_admin);
         $result = $this->db->get('companies', 1);
+        return $result->row();
+    }
+
+
+
+
+    //! ---------------------- CITIES ---------------------- !//
+
+    function getCityById($id_city)
+    {
+        $this->db->where('id_city', $id_city);
+        $result = $this->db->get('cities', 1);
         return $result->row();
     }
 }
